b4d83441ca00b9c7ae8e20d118f8a6ae1e89e019
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-03 14:08:21 +0300

    images and piece posisioning working

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,140 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//images
+import blackBishop from "./assets/images/pieces/black bishop.png";
+import blackRook from "./assets/images/pieces/black rook.png";
+import blackKnight from "./assets/images/pieces/black knight.png";
+import blackQueen from "./assets/images/pieces/black queen.png";
+import blackKing from "./assets/images/pieces/black king.png";
+import blackPawn from "./assets/images/pieces/black pawn.png";
+
+import whiteBishop from "./assets/images/pieces/white bishop.png";
+import whiteRook from "./assets/images/pieces/white rook.png";
+import whiteKnight from "./assets/images/pieces/white knight.png";
+import whiteQueen from "./assets/images/pieces/white queen.png";
+import whiteKing from "./assets/images/pieces/white king.png";
+import whitePawn from "./assets/images/pieces/white pawn.png";
+
+
+type PieceTuple = [string, number];
+
+const pieceNameAndInt: PieceTuple[] = [
+  ["empty", 0],
+  ["king", 1],
+  ["queen", 2],
+  ["rook", 3],
+  ["knight", 4],
+  ["bishop", 5],
+  ["pawn", 6]
+]
+
+
+type PieceImages = {
+  [name: string]: [string, string];
+};
+
+const pieceImages : PieceImages = {
+  "king": [
+    whiteKing,
+    blackKing
+  ],
+  "queen": [
+    whiteQueen,
+    blackQueen
+  ],
+  "rook": [
+    whiteRook,
+    blackRook
+  ],
+  "knight": [
+    whiteKnight,
+    blackKnight
+  ],
+  "bishop": [
+    whiteBishop,
+    blackBishop
+  ],
+  "pawn": [
+    whitePawn,
+    blackPawn
+  ]
+};
+
+function intToPieceName(val: number): [string, boolean] {
+  const isWhite = val>0;
+  val = Math.abs(val);
+  let output = "empty";
+  pieceNameAndInt.forEach((piece) => {
+    if (piece[1] === val) {
+      output = piece[0];
+    }
+  })
+  return [output, isWhite];
+}
+function pieceNameToInt(name: string, isWhite: boolean) {
+  let output = 0;
+  pieceNameAndInt.forEach((piece) => {
+    if (piece[0] === name) {
+      output = piece[1]
+    }
+  })
+  return output * colorMultiplier(isWhite)
+}
+
+function colorMultiplier(isWhite: boolean) {
+  return isWhite && 1 || -1
+}
+
+function mirroedInt(num: number, max: number) {
+  if (num < max/2) {
+    return num
+  }
+  return max-num
+}
+
+function isWhiteFromRow(row: number) {
+  return row < 4
+}
+
+function initialBoardPiece(row: number, col:number) {
+  let isWhite: boolean = isWhiteFromRow(row)
+  let pieceName: string = "empty";
+  const mirrored_row = mirroedInt(row, 7);
+  const mirrored_col = mirroedInt(col, 7);
+
+
+  if (mirrored_row===1) {
+    pieceName = "pawn";
+  } else if (mirrored_row===0) {
+    if (mirrored_col===0) {
+      pieceName = "rook";
+    } else if (mirrored_col===1) {
+      pieceName = "knight";
+    } else if (mirrored_col===2) {
+      pieceName = "bishop"
+    } else if (col === 3) {
+      pieceName = "king"
+    } else if (col === 4) {
+      pieceName = "queen"
+    }
+  }
+
+  return pieceNameToInt(pieceName, isWhite);
+}
+
+
+
+function createMatrix(rows: number, cols: number, initialValueFunction: any): number[][] {
+  let matrix: number[][] = [];
+  for (let i: number = 0; i<rows; i++) {
+    matrix[i] = [];
+    for (let j: number = 0; j<rows; j++) {
+      matrix[i][j] = initialValueFunction(i, j);
+    }
+  }
+  return matrix;
+}
 
 function isLightSquare(row: number, col: number) {
   return row%2 === col%2
@@ -10,8 +144,10 @@ function isLightSquare(row: number, col: number) {
 
 
 function GetSquare({row, col, value}: {row:number, col:number, value:number }) {
+  const [pieceName, isWhite] = intToPieceName(value)
   return (
     <div className={`square_size ${(isLightSquare(row,col) ? "light_square" : "dark_square")}`}>
+      {value !==0 && <img src={pieceImages[pieceName][+ isWhite]} alt={pieceName} className='square_size piece_image' />}
     </div>
   )
 }
@@ -36,16 +172,7 @@ function GetBoard({ board }: { board: number[][]}) {
 
 
 function App() {
-  const [board, setBoard] = useState([
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-  ])
+  const [board, setBoard] = useState(createMatrix(8, 8, initialBoardPiece))
 
   console.log(board)
   return (
