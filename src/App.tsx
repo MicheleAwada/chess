788d1007b12ae72ecbbca55254d6c778b8958157
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-03 13:56:55 +0300

    board working

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,53 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+function isLightSquare(row: number, col: number) {
+  return row%2 === col%2
+}
+
+
+function GetSquare({row, col, value}: {row:number, col:number, value:number }) {
+  return (
+    <div className={`square_size ${(isLightSquare(row,col) ? "light_square" : "dark_square")}`}>
+    </div>
+  )
+}
+
+function GetBoard({ board }: { board: number[][]}) {
+  return (
+    <div id='board'>
+      {
+        board.map((row: number[], i: number) => {
+          return (
+            <div className="row" key={i}>
+              {row.map((col: number, j: number) => {
+                return <GetSquare row={i} col={j} value={col} key={i*8 + j} />
+              })}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
 
 function App() {
+  const [board, setBoard] = useState([
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+  ])
+
+  console.log(board)
   return (
     <>
+      <GetBoard board={board} />
     </>
   )
 }
